4e985ca77617019621b2745eb922e411407b314e
Charles A. Hartman <charles@hartman.dev>
2021-02-25 22:27:24 -0800

    counter assignment

---
+++ counter.rb
@@ -0,0 +1,53 @@
+# counter.rb
+
+require 'socket'
+
+def parse_request(request_line)
+  http_method, path_and_params, _http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = (params || "").split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [http_method, path, params]
+end
+
+server = TCPServer.new('localhost', 3003)
+# rubocop: disable Metrics/BlockLength
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  puts request_line
+
+  next unless request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  client.puts 'HTTP/1.1 200 OK'
+  client.puts "Content-Type: text/html\r\n\r\n"
+
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Counter</h1>"
+
+  number = params["number"].to_i
+  client.puts "<p>The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
+
+  client.puts "</body>"
+  client.puts "</html>"
+
+  client.close
+end
+# rubocop: enable Metrics/BlockLength
